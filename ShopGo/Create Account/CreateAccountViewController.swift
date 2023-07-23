345d5f3b81cd18c93db758151f96a1e3d1252ec5
Ali Ahmed <>
2023-07-22 22:39:38 -0400

    Create Account

---
+++ ShopGo/Create Account/CreateAccountViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CreateAccountViewController.swift
+//  ShopGo
+//
+//  Created by Ali Ahmed on 7/22/23.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        hideKeyboardOnTap()
+        setup()
+    }
+    
+    @objc func dismissMyKeyboard(){
+        view.endEditing(true)
+    }
+    
+    func hideKeyboardOnTap() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissMyKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func setup() {
+        nextButton.layer.shadowColor = UIColor.black.cgColor
+        nextButton.layer.shadowOffset = CGSize(width: 0, height: 7)
+        nextButton.layer.shadowRadius = 5.0
+        nextButton.layer.shadowOpacity = 0.3
+    }
+    
+    
+}
