715224c8ac7268406c0cd11f66d0872828324102
Ali Ahmed <>
2023-07-23 22:28:13 -0400

    Create Account Screen

--- ShopGo/Create Account/CreateAccountViewController.swift
+++ ShopGo/Create Account/CreateAccountViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 class CreateAccountViewController: UIViewController {
     
     
-    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var nextButtonLayout: UIButton!
+    
+    @IBAction func nextButton(_ sender: Any) {
+    }
+    @IBAction func loginButton(_ sender: Any) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +29,10 @@ class CreateAccountViewController: UIViewController {
     }
     
     func setup() {
-        nextButton.layer.shadowColor = UIColor.black.cgColor
-        nextButton.layer.shadowOffset = CGSize(width: 0, height: 7)
-        nextButton.layer.shadowRadius = 5.0
-        nextButton.layer.shadowOpacity = 0.3
+        nextButtonLayout.layer.shadowColor = UIColor.black.cgColor
+        nextButtonLayout.layer.shadowOffset = CGSize(width: 0, height: 7)
+        nextButtonLayout.layer.shadowRadius = 5.0
+        nextButtonLayout.layer.shadowOpacity = 0.3
     }
     
     @objc func dismissMyKeyboard(){
