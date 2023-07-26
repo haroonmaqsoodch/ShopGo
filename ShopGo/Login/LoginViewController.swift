02024a405184aa2327eed866e2bbdf1fe771c0d5
Haroon Chaudhry <haroonchmaqsood@gmail.com>
2023-07-26 17:56:57 -0400

    add login vc

---
+++ ShopGo/Login/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  GoToShop
+//
+//  Created by Waqas Shabir on 7/23/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userNameTextField.iconInsideTextField(imageViewNamed: "username")
+        passwordTextField.iconInsideTextField(imageViewNamed: "password")
+        }
+    
+    @IBAction func loginButtonAction(_ sender: UIButton) {
+    }
+}
