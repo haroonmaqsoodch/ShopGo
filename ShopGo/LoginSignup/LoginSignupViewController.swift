27894231f92694b725d15e49c9a279013251ee3c
Haroon Chaudhry <haroonchmaqsood@gmail.com>
2023-07-26 17:53:42 -0400

    create login signup screen

---
+++ ShopGo/LoginSignup/LoginSignupViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SignupScreenViewController.swift
+//  ShopGo
+//
+//  Created by Zoha Nadeem on 7/23/23.
+//
+
+import UIKit
+
+class LoginSignupViewController: UIViewController {
+    
+    @IBOutlet weak var buttonView: UIView!
+    @IBOutlet weak var shopGoImage: UIImageView!
+    @IBOutlet weak var createButtonOutlet: UIButton!
+    @IBOutlet weak var loginButtonOutlet: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func createAccoutTap(_ sender: Any) {
+    }
+    @IBAction func LoginButtonTap(_ sender: Any) {
+    }
+}
