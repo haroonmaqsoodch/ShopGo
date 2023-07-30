da69b60b08c63980bddad924ecc4383414113c52
Waqas Shabir <>
2023-07-30 12:08:41 -0400

    Implement restriction on login screen

--- ShopGo/Login/LoginViewController.swift
+++ ShopGo/Login/LoginViewController.swift
@@ -7,18 +7,27 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
-
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var forgotPassword: UILabel!
     @IBOutlet weak var passwordTextField: UITextField!
+    
+    var userName: String = "Shopgo321"
+    var passWord: String = "Apple321!"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        userNameTextField.iconInsideTextField(imageViewNamed: "username")
-        passwordTextField.iconInsideTextField(imageViewNamed: "password")
-        }
+        userNameTextField.iconInsideTextField(imageViewNamed: "Username")
+        passwordTextField.iconInsideTextField(imageViewNamed: "Password")
+        hideKeyboardOnTap()
+        passwordTextField.delegate = self
+    }
     
     @IBAction func loginButtonAction(_ sender: UIButton) {
+        if userNameTextField.text != userName || passwordTextField.text != passWord {
+            wrongPasswordShaker()
+        }
     }
 }
