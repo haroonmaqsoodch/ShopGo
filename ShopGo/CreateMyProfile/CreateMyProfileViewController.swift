db3b3a5c17c5ce0da9fed4941687bc6dd4ac1dd3
Zoha Nadeem <>
2023-08-04 03:04:26 +0500

    Create My Profile Screen

---
+++ ShopGo/CreateMyProfile/CreateMyProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CreateMyProfileViewController.swift
+//  ShopGo
+//
+//  Created by Zoha Nadeem on 8/3/23.
+//
+
+import UIKit
+
+class CreateMyProfileViewController: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var address: UIImageView!
+    @IBOutlet weak var order: UIImageView!
+    @IBOutlet weak var setting: UIImageView!
+    @IBOutlet weak var home: UIImageView!
+    @IBOutlet weak var cart: UIImageView!
+    @IBOutlet weak var profile: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        profilePictureUI()
+        loadIconImages()
+    }
+}
