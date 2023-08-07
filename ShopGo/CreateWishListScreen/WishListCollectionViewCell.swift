c1eb64359cf50ce3e91fa2fd0938b0acea2ae7d8
Abdul wali Latif <>
2023-08-07 16:59:17 -0400

    Create Wishlist Screen

---
+++ ShopGo/CreateWishListScreen/WishListCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  WishListCollectionViewCell.swift
+//  ShopGo
+//
+//  Created by Abdul wali Latif on 8/7/23.
+//
+
+import UIKit
+
+class WishListCollectionViewCell: UICollectionViewCell {
+   
+    @IBOutlet weak var cellLabel: UILabel!
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var moneyLabel: UILabel!
+}
---
+++ ShopGo/CreateWishListScreen/WishListViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WishListViewController.swift
+//  ShopGo
+//
+//  Created by Abdul wali Latif on 8/7/23.
+//
+
+import UIKit
+
+class WishListViewController: UIViewController {
+    
+    var wishListLabelArray: [String] = ["Watch","Heels","Shoes" ,"Shoes", "Bag", "Dress", "Watch", "Bag", "Shoes", "Dress"]
+     var wishListMoneyArray: [String] = ["20$"]
+     var wishListImageArray = [
+         UIImage(named: "watch"),
+         UIImage(named: "heel"),
+         UIImage(named: "shoes"),
+         UIImage(named: "shoes1"),
+         UIImage(named: "bag"),
+         UIImage(named: "dress"),
+         UIImage(named: "watch1"),
+         UIImage(named: "bag1"),
+         UIImage(named: "sandal"),
+         UIImage(named: "dress1")]
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var numberOfItemLabel: UILabel!
+    @IBAction func ellipsisButton(_ sender: Any) {
+    }
+    @IBOutlet weak var ellipsisButtonOutlet: UIButton!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpWishListButton()
+    }
+}
