2b39645d233bd7cdbad5fbdc3d973a7484658179
05Waleed <139551937+05Waleed@users.noreply.github.com>
2023-08-12 21:44:27 +0200

    fix menu animation

--- ShopGo/HomeScreen/ContainerVeiwExtension.swift
+++ ShopGo/HomeScreen/ContainerVeiwExtension.swift
@@ -10,23 +10,11 @@ import UIKit
 extension HomeViewController {
     
     func openAndHideContainerView() {
-        if containerView.isHidden {
+        containerView.isHidden = !containerView.isHidden
+        if containerView.isHidden == false {
             title = ""
-            let frame = containerView.frame
-            containerView.frame = CGRect(x: -frame.size.width, y: frame.origin.y, width: frame.size.width, height: frame.size.height)
-            containerView.isHidden = false
-            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
-                self.containerView.frame = frame
-            }, completion: nil)
         } else {
-            let frame = containerView.frame
             title = "ShopGo"
-            UIView.animate(withDuration: 0.5, animations: {
-                self.containerView.frame = CGRect(x: -frame.size.width, y: frame.origin.y, width: frame.size.width, height: frame.size.height)
-            }) { _ in
-                self.containerView.frame = frame
-                self.containerView.isHidden = true
-            }
         }
     }
 }
